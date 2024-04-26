7acdb9af00639c031cb189aa844b1ea4be2414b7
Ayyaz1122 <azmarshal312@gmail.com>
2024-04-26 14:36:33 -0500

    update please

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from . import counter
 
 def home(request):
-    return render(request, 'index.html', {'key1': 'I am coming from Python code'})
+    return render(request, 'index.html', {'key1': 'I am coming from Python not code'})
 
 def result(request):
     article = request.GET ['article']
