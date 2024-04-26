193600aaf7343e0b34f69bd7530f65321d822023
Ayyaz1122 <azmarshal312@gmail.com>
2024-04-26 14:34:10 -0500

    add setup od new project of python

---
+++ djangoProject/views.py
@@ -0,0 +1,12 @@
+import operator
+
+from django.shortcuts import render
+from . import counter
+
+def home(request):
+    return render(request, 'index.html', {'key1': 'I am coming from Python code'})
+
+def result(request):
+    article = request.GET ['article']
+    var_dict, word_count = counter.counter(article)
+    return render(request, 'result.html', {'article': article, 'word_count': word_count, 'dict_words': var_dict})
